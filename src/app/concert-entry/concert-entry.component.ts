3c950c518e8bc9b742100fd7921decadd04c2105
MaeveRositzke <maeverositzke@gmail.com>
2025-02-19 21:31:28 -0500

    Started concert entry component

--- src/app/concert-entry/concert-entry.component.ts
+++ src/app/concert-entry/concert-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-concert-entry',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './concert-entry.component.css'
 })
 export class ConcertEntryComponent {
+  @Input() user: string = '';
+  @Input() date: string = '';
+  @Input() artists: string[] = [];
+  @Input() media: string[] = [];
+  @Input() rating: string = '';
+  @Input() description: string = '';
+  @Input() venue: string = '';
+  @Input() city: string = '';
+  @Input() tour: string = '';
+  @Input() comments: string = '';
+  @Input() likes: number = 0;
 
+  likeConcert() {
+    this.likes++;
+  }
 }
